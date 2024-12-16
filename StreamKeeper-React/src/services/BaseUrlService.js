f6c664f08afcb0d0d9ac11d3c8e9e51f503c0410
PandaFlo <Flashmait@hotmail.com>
2024-12-16 16:03:01 -0500

    url flexability

--- StreamKeeper-React/src/services/BaseUrlService.js
+++ StreamKeeper-React/src/services/BaseUrlService.js
@@ -3,11 +3,12 @@ const PUBLIC_IP = '';  // Leave this blank, fill in when needed
 export class BaseUrlService {
   getBaseUrl(port, endpoint = '') {
     const baseIP = PUBLIC_IP.trim() || 'localhost';
+    const protocol = PUBLIC_IP.trim() ? 'https' : 'http';
     
     if (!endpoint) {
-      return `http://${baseIP}:${port}/api`;
+      return `${protocol}://${baseIP}:${port}/api`;
     }
     
-    return `http://${baseIP}:${port}/api/${endpoint}`;
+    return `${protocol}://${baseIP}:${port}/api/${endpoint}`;
   }
 }
